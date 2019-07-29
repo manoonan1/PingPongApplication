d55abe4c44cd0bf0b0faa40a3b3da6c2a673fac9
manoonan1 <mnoonan136@gmail.com>
2019-07-28 22:53:50 -0400

    Added Login UI - Routes to Menu UI

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,26 +1,87 @@
-
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
 
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold( // 1
-      appBar: new AppBar( //2
-        title: new Text("Login"),
-      ),
-      body: new Container(
-      // decoration: new BoxDecoration(color: Colors.white),
-        child: new Center(
-          child: 
-            RaisedButton(
-              child: Text("Menu"),
-              onPressed: () {
-                Navigator.pushNamed(context, '/menu');
-            }
-          )
-        )
-      )
-    );
+    final TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+    @override
+    Widget build(BuildContext context) {
+      FocusNode emailFocusNode = new FocusNode();
+
+      TextFormField emailField = new TextFormField(
+        onFieldSubmitted: (String value) {
+          FocusScope.of(context).requestFocus(emailFocusNode);
+        },
+        autofocus: false,
+        obscureText: false,
+        style: style,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Email",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      );
+      TextFormField passwordField = new TextFormField(
+        //focusNode: emailFocusNode,
+        obscureText: true,
+        style: style,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Password",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      );
+      final loginButon = Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(30.0),
+        color: Color(0xff01A0C7),
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          onPressed: () {
+            Navigator.pushNamed(context, '/menu');
+          },
+          child: Text("Login",
+              textAlign: TextAlign.center,
+              style: style.copyWith(
+                  color: Colors.white, fontWeight: FontWeight.bold)),
+        ),
+      );
+
+      return Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Center(
+          child: Container(
+            color: Colors.blue[100],
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 180.0,
+                    child: Image.asset(
+                      "assets/logo.png",
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  SizedBox(height: 45.0),
+                  emailField,
+                  SizedBox(height: 25.0),
+                  passwordField,
+                  SizedBox(
+                    height: 35.0,
+                  ),
+                  loginButon,
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
   }
-}
